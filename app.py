3bae986d52db0d6ca04e6036962f20d0e4755b26
AvaTar ArTs <sjchaplinski@gmail.com>
2024-08-08 15:09:31 -0400

    Initial commit

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
 
+app = Flask(__name__)
+api = Api(app)
+
+class CreateScript(Resource):
+    def post(self):
+        data = request.get_json()
+        if not data:
+            return {"message": "Invalid request"}, 400
+
+        # Process the data (this is where your script creation logic will go)
+        script_id = "12345"  # Placeholder for created script ID
+        title = data.get("title")
+
+        return jsonify({
+            "id": script_id,
+            "title": title,
+            "message": "Script created successfully"
+        })
+
+api.add_resource(CreateScript, '/createScript')
+
+if __name__ == '__main__':
+    app.run(debug=True)
